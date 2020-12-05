3707c55b45aeb23de65be4d58bbcfd0f6f3e5b15
Anna Kijania <kijania.anna@gmail.com>
2020-12-05 20:22:32 +0100

    Fix router.put and router.delete

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -52,7 +52,7 @@ router.put('/departments/:id', async (req, res) => {
     if (dep) {
       dep.name = name;
       await dep.save();
-      res.json(await Department.findById(req.params.id));
+      res.json(dep);
     } else res.status(404).json({ message: 'Not found...' });
   } catch (err) {
     res.status(500).json({ message: err });
@@ -64,7 +64,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if (dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json(await Department.find());
+      res.json(dep);
     } else res.status(404).json({ message: 'Not found...' });
   } catch (err) {
     res.status(500).json({ message: err });
